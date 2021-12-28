d3460a8a141176195f5a61b168b904bb2635bff1
Tulika Das <tulikadas1029@gmail.com>
2021-12-28 21:04:06 +0530

    some changes

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -2,11 +2,23 @@ import { tns } from 'tiny-slider/src/tiny-slider'
 
 if (document.querySelector('.unicorn-hero-section')) {
     tns({
-        container:'.unicorn-hero-section .slides',
+        container: '.unicorn-hero-section .slides',
         items: 1,
         nav: true,
         autoplay: true,
         autoplayButtonOutput: false,
-        controls:false,
+        controls: false,
+    })
+}
+
+if (document.querySelector('.scl-parents-sec')) {
+    tns({
+        container: '.scl-parents-sec .slides',
+        items: 1,
+        nav: false,
+        controlsContainer: '.scl-parents-sec .parent-nav',
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoHeight: true,
     })
 }
