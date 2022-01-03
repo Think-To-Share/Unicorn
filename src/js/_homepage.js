c18ae63be20e98d668f5edc3fe2e8a05591010be
Rahul Dey <rahul900day@gmail.com>
2022-01-03 20:31:29 +0530

    changed slider behaviour

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -1,13 +1,43 @@
 import { tns } from 'tiny-slider/src/tiny-slider'
 
 if (document.querySelector('.unicorn-hero-section')) {
-    tns({
+    // hide all hero text
+    const heroText = document.querySelectorAll('.unicorn-hero-text p')
+    heroText.forEach(text => {
+        text.style.display = 'none'
+    })
+
+    // slider
+    const heroSlider = tns({
         container: '.unicorn-hero-section .slides',
         items: 1,
         nav: true,
         autoplay: true,
         autoplayButtonOutput: false,
         controls: false,
+    })
+
+    // show first hero text
+    heroText[0].style.display = 'block'
+
+    // show hero text on slide change
+    heroSlider.events.on('indexChanged', (info) => {
+        const text = heroText[info.index - 1]
+
+        if(text) {
+            heroText.forEach(text => {
+                text.style.display = 'none'
+            })
+
+            text.style.display = 'block'
+
+            // animate hero text
+            text.classList.add('animate__animated', 'animate__slideInRight')
+            text.style.setProperty('--animate-duration', '0.5s');
+            text.addEventListener('animationend', () => {
+                text.classList.remove('animate__animated', 'animate__slideInRight');
+            })
+        }
     })
 }
 
